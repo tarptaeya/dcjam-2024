989f529a0d40f1f26a2b581e53fc17c31458d59f
Anmol Gautam <tarptaeya@gmail.com>
2024-03-27 16:40:29 -0700

    added spotlight to player

--- src/Game.js
+++ src/Game.js
@@ -11,6 +11,8 @@ const Game = () => {
   const playerLocation = useSelector((state) => state.playerLocation.value);
   const playerDirection = useSelector((state) => state.playerDirection.value);
 
+  const spotLightRef = useRef();
+
   useEffect(() => {
     camera.fov = 100;
     camera.updateProjectionMatrix();
@@ -33,11 +35,16 @@ const Game = () => {
     const deltaPosition = vectorForDirection(playerDirection);
     const front = new Vector3(camera.position.x + deltaPosition.x, camera.position.y + deltaPosition.y, camera.position.z + deltaPosition.z);
     camera.lookAt(front);
+
+    spotLightRef.current.position.x = camera.position.x;
+    spotLightRef.current.position.y = camera.position.y;
+    spotLightRef.current.position.z = camera.position.z;
   });
 
   return (
     <>
-      <ambientLight intensity={5.0} />
+      <ambientLight intensity={0.1} />
+      <pointLight ref={spotLightRef} castShadow={true} />
       <Dungeon />
     </>
   );
